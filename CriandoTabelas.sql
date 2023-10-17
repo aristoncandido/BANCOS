5b8c2fb8422ca930e81f4f4b50bd828d549bc404
Ariston Cândido <97634221+aristoncandido@users.noreply.github.com>
2023-10-17 13:11:25 -0300

    tirei o coment

--- CriandoTabelas.sql
+++ CriandoTabelas.sql
@@ -1,11 +1,11 @@
-/*create table escolas(id int primary key not null  auto_increment,nome varchar(50),endereco text(200));*/
+create table escolas(id int primary key not null  auto_increment,nome varchar(50),endereco text(200));
 
-/*insert into escolas VALUES(1,'Sizenando Silveira','Av. Agamenon Magalhães');*/
+insert into escolas VALUES(1,'Sizenando Silveira','Av. Agamenon Magalhães');
 
-/*INSERT INTO escolas VALUES(2,'ETE - Cicero Dias','Rua barão de souza leão');*/
+NSERT INTO escolas VALUES(2,'ETE - Cicero Dias','Rua barão de souza leão');
 
-/*INSERT INTO escolas VALUES(4,'scola Brigadeiro','Rua barão');*/
-/*INSERT INTO escolas VALUES(5,'cola Brigadeiro','Rua barão');*/
+INSERT INTO escolas VALUES(4,'scola Brigadeiro','Rua barão');
+INSERT INTO escolas VALUES(5,'cola Brigadeiro','Rua barão');
 
 
 
