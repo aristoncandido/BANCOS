8d1a521c4ddbf6a92bd49ac316a412e561fece41
Ariston Cândido da Silva Filho <ariston.filho@coren-pe.com.br>
2023-10-17 12:20:59 -0300

    meus bancos

---
+++ CriandoTabelas.sql
@@ -0,0 +1,31 @@
+/*create table escolas(id int primary key not null  auto_increment,nome varchar(50),endereco text(200));*/
+
+/*insert into escolas VALUES(1,'Sizenando Silveira','Av. Agamenon Magalhães');*/
+
+/*INSERT INTO escolas VALUES(2,'ETE - Cicero Dias','Rua barão de souza leão');*/
+
+/*INSERT INTO escolas VALUES(4,'scola Brigadeiro','Rua barão');*/
+/*INSERT INTO escolas VALUES(5,'cola Brigadeiro','Rua barão');*/
+
+
+
+
+
+
+select * from escolas WHERE id > 2 && id  < 5;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
